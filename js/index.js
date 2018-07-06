ecc3fcc3e26534d84f030e228649bda9f557e785
Aaron Villanueva <aarone.villanueva@gmail.com>
2018-07-05 23:59:19 -0700

    adds plan

---
+++ js/index.js
@@ -0,0 +1,21 @@
+console.log('JS is working!!')
+
+//create quiz app using object constructor notation and prototype methods
+//constructor will take an object  with question answer as key value pair
+//key would be the question
+//value would be the answer
+//first create constructor and prototype
+//then add methods to render the q's
+//if user input answers match the key's value then turn to green
+//this will be the plan for now just to keep quiz app simpe
+
+
+
+function createQuiz() {
+  this.questions = []
+}
+
+
+quiz1 = {
+
+}
