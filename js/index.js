c4bc8273ea774aa42fc55ad8ed84a1a5e7f4c339
Aaron Villanueva <aarone.villanueva@gmail.com>
2018-07-06 00:51:34 -0700

    ""[D

--- js/index.js
+++ js/index.js
@@ -16,22 +16,38 @@ function Quiz(quizAsObj) {
 }
 
 Quiz.prototype.renderQs = function() {
+  //questions created through constructor function
   var qs = this.questions
+  //get ul 
+  var listQs = document.getElementById('questions')
+  //render questions as list and input
   for (var i = 0; i < qs.length; i++) {
-    //get ul 
     //create li 
-    //create 
+    var li = document.createElement('li')
+    //create input
+    var input = document.createElement('input')
     //add q's as text content and render questions
+    li.textContent = qs[i]
     //create input in li
-    //
+    li.appendChild(input)
+    //append create elements to ul
+    listQs.appendChild(li)
   }
 }
 
 Quiz.prototype.checkAns = function() {
   //gets all the user inputs
+  var guesses = document.getElementsByTagName('input')
   //check to see if they match with answers
-  //if it does then change something to gree
-  //if it doesn't then change it to red
+  for (i = 0; i < this.answers.length; i++) {
+  //if it does then change background color to green
+    if(guesses[i].value == this.answers[i]) {
+      guesses[i].style.backgroundColor = "green"
+    } else {
+  //else changes background color it to red
+      guesses[i].style.backgroundColor= "red"
+    }
+  }
 }
 
 var simpleMath = new Quiz(
@@ -43,8 +59,16 @@ var simpleMath = new Quiz(
   }
 )
 
-
 simpleMath.renderQs()
 
+document.getElementById('form').addEventListener('submit',
+  function(event){
+    event.preventDefault()
+    simpleMath.checkAns()
+  }
+)
 
-document.getElementById('submit', simpleMath.checkAns())
+
+
+
+
